b216ea522232fc173ef88527f5937cd5d1f7813a
Linh Do <108320137+dophuonglinhh@users.noreply.github.com>
2026-09-07 23:52:11 +0700

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {boolean}
+     */
+    containsNearbyDuplicate(nums, k) {
+        if (nums.length == 1 || k == 0)  return false;
+
+        const set = new Set();
+
+        for (let i = 0; i < nums.length; i++) {
+            if (set.has(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+
+            if (set.size > k) {
+                set.delete(nums[i - k]);
+            }
+        }
+
+        return false;
+    }
+}
